35ea4b9db41c90a3c6c4c13f95e16a26b18a55b0
CaroDiego <dicafre1404@gmail.com>
2025-02-10 11:45:09 +0100

    film detail style

--- src/components/FilmDetail.jsx
+++ src/components/FilmDetail.jsx
@@ -4,6 +4,7 @@ import { Favorite } from "@mui/icons-material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
 import { FilmContext } from "../context/film.context";
+import AddToList from "./AddToList";
 
 function FilmDetail(props) {
   const { changeLiked, changeWatched } = useContext(FilmContext);
@@ -56,6 +57,17 @@ function FilmDetail(props) {
     setWatch(!watch);
   };
 
+  const [seen, setSeen] = useState(false);
+
+  function togglePop() {
+    setSeen(!seen);
+    if (!seen) {
+      document.body.classList.add("popup-active");
+    } else {
+      document.body.classList.remove("popup-active");
+    }
+  }
+
   if (!film) {
     return <p className="loading">Loading...</p>;
   }
@@ -70,43 +82,63 @@ function FilmDetail(props) {
         alt={film?.name}
         className="film-detail-poster"
       />
+      <div className="film-content">
+        <div className="film-info-actions">
+          <div className="film-info">
+            <span className="film-title-year">
+              <h1 className="film-title">{film?.name}</h1>
+              <h2 className="film-year">{film?.year}</h2>
+            </span>
+            <div className="film-directors">
+              {directors.map((director) => (
+                <span className="film-director" key={director}>
+                  {director}
+                </span>
+              ))}
+            </div>
+            <div className="film-genres">
+              {genres.map((genre) => (
+                <span className="film-genre" key={genre}>
+                  {genre}
+                </span>
+              ))}
+            </div>
+          </div>
 
-      {/* Información en el centro */}
-      <div className="film-info">
-        <h1 className="film-title">{film?.name}</h1>
-        <h2 className="film-year">{film?.year}</h2>
-        <div className="film-genres">
-          {genres.map((genre) => (
-            <span className="film-genre" key={genre}>
-              {genre}
+          <div className="film-actions">
+            <span className="film-actions-icons">
+              <button
+                onClick={handleWatchChange}
+                className={watch ? "watched" : ""}
+              >
+                <VisibilityIcon />
+              </button>
+              <button
+                onClick={handleLikeChange}
+                className={like ? "liked" : ""}
+              >
+                <Favorite />
+              </button>
             </span>
-          ))}
+
+            <button className="add-to-list-btn" onClick={togglePop}>
+              ➕ Añadir a lista
+            </button>
+          </div>
         </div>
-        <div className="film-directors">
-          {directors.map((director) => (
-            <span className="film-director" key={director}>
-              {director}
-            </span>
-          ))}
+
+        {/* Sinopsis debajo de la información y acciones */}
+        <div className="film-synopsis">
+          <h3>Sinopsis</h3>
+          <p>
+            A television reporter and cameraman follow emergency workers into a
+            dark apartment building and are quickly locked inside with something
+            terrifying.
+          </p>
         </div>
       </div>
 
-      {/* Estado y botón a la derecha */}
-      <div className="film-actions">
-        <button onClick={handleWatchChange} className={watch ? "watched" : ""}>
-          <VisibilityIcon />
-        </button>
-        <button onClick={handleLikeChange} className={like ? "liked" : ""}>
-          <Favorite />
-        </button>
-        <span className="film-watched">
-          {film?.watched ? "✅ Vista" : "❌ No vista"}
-        </span>
-        <span className="film-liked">
-          {film?.liked ? "❤️ Me gustó" : "💔 No me gustó"}
-        </span>
-        <button className="add-to-list-btn">➕ Añadir a lista</button>
-      </div>
+      {seen ? <AddToList film={film} toggle={togglePop}></AddToList> : null}
     </div>
   );
 }
